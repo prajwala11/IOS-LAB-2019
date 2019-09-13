cf6d5d346eb9a3193968348518fa7cd47bd415d8
prajwala11 <55274539+prajwala11@users.noreply.github.com>
2019-09-13 15:28:32 +0530

    Add files via upload

---
+++ fp.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+	printf(1,"wow!!!!\n");
+	printf(1,"not fun\n");
+	exit();
+}
---
+++ myprog.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+	printf(1,"my program in xv6\n");
+	exit();
+}
+
